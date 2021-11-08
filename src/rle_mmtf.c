6342234b51c387ee20c9490f77a3e12489c97a43
Christoph Stiller <c.stiller@live.de>
2021-11-08 22:48:54 +0100

    AVX Implementation, but it's still way too slow

--- src/rle_mmtf.c
+++ src/rle_mmtf.c
@@ -16,45 +16,45 @@ uint32_t rle_mmtf_compress(IN const uint8_t *pIn, const uint32_t inSize, OUT uin
   if (inSize > outSize)
     return 0;
 
-  ALIGN(128) uint8_t history[16 * 256];
+  ALIGN(128) uint8_t history[sizeof(__m256i) * 256];
 
   for (size_t i = 0; i < 256; i++)
-    _mm_store_si128(history + i * sizeof(__m128i), _mm_set1_epi8((char)i));
+    _mm256_store_si256(history + i * sizeof(__m256i), _mm256_set1_epi8((char)i));
 
   uint32_t inSizeRemaining = inSize;
 
-  while (inSizeRemaining >= sizeof(__m128i))
+  while (inSizeRemaining >= sizeof(__m256i))
   {
-    inSizeRemaining -= sizeof(__m128i);
+    inSizeRemaining -= sizeof(__m256i);
 
-    const __m128i symbols = _mm_loadu_si128(pIn); // could be stream load with sse 4.1.
-    pIn += sizeof(__m128i);
+    const __m256i symbols = _mm256_loadu_si256(pIn); // could be stream load with sse 4.1.
+    pIn += sizeof(__m256i);
 
-    __m128i currentHist = _mm_load_si128(history);
-    __m128i matched = _mm_cmpeq_epi8(symbols, currentHist);
-    __m128i out = _mm_and_si128(matched, _mm_setzero_si128());
-    __m128i prevMatched = matched;
-    __m128i history0 = _mm_and_si128(matched, currentHist);
-    __m128i lastHist;
+    __m256i currentHist = _mm256_load_si256(history);
+    __m256i matched = _mm256_cmpeq_epi8(symbols, currentHist);
+    __m256i out = _mm256_and_si256(matched, _mm256_setzero_si256());
+    __m256i prevMatched = matched;
+    __m256i history0 = _mm256_and_si256(matched, currentHist);
+    __m256i lastHist;
 
     for (size_t i = 1; i < 256; i++)
     {
       lastHist = currentHist;
-      currentHist = _mm_load_si128(history + i * sizeof(__m128i));
-      matched = _mm_cmpeq_epi8(symbols, currentHist);
-      out = _mm_or_si128(out, _mm_and_si128(matched, _mm_set1_epi8(i)));
+      currentHist = _mm256_load_si256(history + i * sizeof(__m256i));
+      matched = _mm256_cmpeq_epi8(symbols, currentHist);
+      out = _mm256_or_si256(out, _mm256_and_si256(matched, _mm256_set1_epi8(i)));
 
       // Overwrite History With Last History If Previously Not Matched, Or The Current History.
-      history0 = _mm_or_si128(history0, _mm_and_si128(matched, currentHist));
-      _mm_store_si128(history + i * sizeof(__m128i), _mm_or_si128(_mm_andnot_si128(prevMatched, lastHist), _mm_and_si128(prevMatched, currentHist)));
+      history0 = _mm256_or_si256(history0, _mm256_and_si256(matched, currentHist));
+      _mm256_store_si256(history + i * sizeof(__m256i), _mm256_or_si256(_mm256_andnot_si256(prevMatched, lastHist), _mm256_and_si256(prevMatched, currentHist)));
 
-      prevMatched = _mm_or_si128(prevMatched, matched);
+      prevMatched = _mm256_or_si256(prevMatched, matched);
     }
 
-    _mm_store_si128(history, history0);
-    _mm_store_si128(pOut, out);
+    _mm256_store_si256(history, history0);
+    _mm256_store_si256(pOut, out);
 
-    pOut += sizeof(__m128i);
+    pOut += sizeof(__m256i);
   }
 
   for (size_t i = 0; i < inSizeRemaining; i++)
